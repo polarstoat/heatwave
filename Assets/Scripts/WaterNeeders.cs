3e2d3966b6fc228de180d31dfe1fa1a776cfd022
jamesdevlinpilbrow <41115000+jamesdevlinpilbrow@users.noreply.github.com>
2018-07-11 16:24:05 +0100

    Add water needer script
    Basic script. Has state of decay and death trigger. Should be easy for children to use

--- Assets/Scripts/WaterNeeders.cs
+++ Assets/Scripts/WaterNeeders.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class WaterNeeders : MonoBehaviour {
 
+
+    protected int currentTemp = 0;
+    protected int witherTemp = 0;
+    protected int maxTemp = 0;
+    protected bool isLive = true;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        DecayState();
+    }
+
+
+    // Update is called once per frame
+    void  Update()
+    {
+        currentTemp++;
+        while (isLive == true)
+        {
+            DecayState();
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    protected virtual void DecayState()
+    {
+        if (currentTemp < witherTemp)
+        {
+
+        }
+        else if (currentTemp >= witherTemp && currentTemp < maxTemp)
+        {
+
+        }
+        else if (currentTemp >= maxTemp)
+        {
+            isLive = false;
+        }
+    }
 }
