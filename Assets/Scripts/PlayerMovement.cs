453cc4acb023025bab2bee5944e6725c6a6f3d82
Polar stoat <polarstoat@users.noreply.github.com>
2018-07-11 15:46:34 +0100

    Add placeholder character (that moves like spaceship)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float Speed;
+
+    private void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion newRotation = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        transform.rotation = newRotation;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+        float input = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * Speed * input);
+    }
+}
