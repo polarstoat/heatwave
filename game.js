e9e6024d836dbaa12215a184431ca2e294e27bdc
Polar stoat <polarstoat@users.noreply.github.com>
2018-07-12 11:29:22 +0100

    Add JS scaffolding

---
+++ game.js
@@ -0,0 +1,22 @@
+const game = new Phaser.game({
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+});
+
+function preload() {
+  // do work
+}
+
+function create() {
+  // do work
+}
+
+function update() {
+  // do work
+}
